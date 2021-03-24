a99344765eb33a1d2890698c8bb496c38b6b59c2
angelpb0904 <angelpb0904@gmail.com>
2021-03-23 23:17:44 -0400

    Instalar paquetes Yargs y Colors

--- debugging-node/index.js
+++ debugging-node/index.js
@@ -1,30 +1,30 @@
-    /*  Algoritmo de ordenamiento de al Burbuja 0(n`2)*/
+/* Algoritmo de ordenamiento de la Burbuja 0(n^2)*/
 
-const ordenaBurbuja = (lista) => {
+const ordenaBurbuja = (lista) => { 
 
 
-    let longitudlista, indexOrder, indexChange, auxChange;
-    longitudlista = Lista.length;
+    let longitudLista, indexOrder, indexChage, auxChange;
+    longitudLista = lista.length;
 
-    console.info(longitudlista);  // Mostramos por consola la cantidad de elementos en la lista
+    console.info(longitudLista); // Mostramos por consola la cantidad de elementos en la lista.
 
-    for (indexOrder = 1; indexOrder < longitudlista; index0rder++) {
-         //For animado.
-         for (indexChange = 0; indexChange < (longitudlista - indexOrder); indexChange++) {
-             // Intercambiamos los valores si el anterior es mayor al siguiente. 
-             if (lista[indexChange] > lista[indexChange + 1]) {
-                  auxChange = lista [indexChange];
-                  lista[indexChange] = lista[indexChange + 1];
-                  lista[indexChange + 1] = auxChange; 
+    for (indexOrder = 1; indexChage < longitudLista; indexOrder++) {
+        //For anidado.
+        for (indexChage = 0; indexChage < (longitudLista - indexOrder); indexChage++) {
+            //Intercambiamos los valores si el anterior esmayor al siguiente.
+            if (lista[indexChage] > lista [indexChage + 1]) {
+                auxChange = lista [indexChage];
+                lista[indexChage] = lista[indexChage + 1];
+                lista[indexChage + 1] = auxChange;
             }
         }
-     }
+    }
 
-    return lista; 
+    return lista;
 }
 
 let listaDesordenada = [6, 5, 3, 1, 8, 7, 2, 4];
-console.warn(listaDesordenada); // Mostramos. por consola, la lista desordenada
+console.warn(listaDesordenada); // Mostramos, por consola, la lista desordenada.
 
 const listaOrdenada = ordenaBurbuja(listaDesordenada);
-console.log(1, 2, 3, 4, 5, 6, 7, 8); // Mostramos, por consola, la lista ya ordenada. 
+console.log(listaOrdenada); //Mostramos, por consola, la lista ordenada.
